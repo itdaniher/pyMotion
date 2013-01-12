77377319ab91583e8d75ce08285b2e04732fb040
Ian Daniher <it.daniher@gmail.com>
2013-01-12 17:40:17 -0500

    added faster getImage function

---
+++ getFrameFromVideoStream.py
@@ -0,0 +1,34 @@
+import Image
+import time, urllib2, base64
+
+
+def getImageFast():
+	# Basic HTTP Authentication...
+	url = 'http://192.168.1.116/videostream.cgi?user=admin&resolution=C&rate=6'
+	ww = 'admin:'
+	encodedstring = base64.encodestring(ww)[:-1]
+	auth = "Basic %s" % encodedstring
+	req = urllib2.Request(url,None, {"Authorization": auth })
+	handle = urllib2.urlopen(req)
+	
+	def read_stream():
+		buf = ''
+		b = handle.readlines(45)
+		for a in b:
+			if a.startswith('Content-Length'):
+				readlen = str(a).split()[1]
+				print readlen
+		b1 = handle.read(int(readlen)+4)
+		return b1
+	
+	def parse_stream():
+		imgc = read_stream()
+		buf = Image.fromstring('RGB',(640,480),imgc[2:], 'jpeg', 'RGB', None)
+		return buf
+
+	return parse_stream()
+
+if __name__ == "__main__":
+	while True:
+		getImageFast().show()
+		time.sleep(1)
