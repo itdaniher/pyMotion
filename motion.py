16bc51bd10e221a2244f1187e8e7cfac3fc90062
Ian Daniher <it.daniher@gmail.com>
2013-01-12 16:46:38 -0500

    split into functions

--- motion.py
+++ motion.py
@@ -15,16 +15,25 @@ def getImage():
 	""" returns a 480x640 numpy array containing an image from a FOSCAM IP camera """
 	imageFile = StringIO.StringIO(requests.get("http://192.168.1.116/snapshot.cgi?user=admin&pwd=").content)
 	jpg = Image.open(imageFile)
+	# ITU-R 601-2 luma
 	jpg = jpg.convert("L")
+	return jpg
+
+def ImageToArray(jpg):
 	a = numpy.fromstring(jpg.tostring(), "uint8").reshape((480,640))
 	return a
 
-while True:
-	a = getImage()
-	b = getImage()
-	# smooth images
+def getSmoothedArray():
+	a = ImageToArray(getImage())
 	af = filterfft.filter(a, filterfft.gaussian(2))
-	bf = filterfft.filter(b, filterfft.gaussian(2))
+	return af
+
+def viewArray(a):
+	Image.fromarray(a).show()
+
+def oneTick():
+	af = getSmoothedArray()
+	bf = getSmoothedArray()
 	# subtract images
 	diff = numpy.abs(numpy.sum(bf-af))
 	if diff > threshold:
@@ -33,3 +42,11 @@ while True:
 		Image.fromarray(a.astype("uint8")).save(fa)
 		Image.fromarray(b.astype("uint8")).save(fb)
 	print diff
+
+if __name__ == "__main__":
+	while True:
+		try:
+			oneTick()
+		except:
+			print "napping"
+			time.sleep(10)
